fec586bb7237cab4e24e6d9b3ac245f3e7a94079
isabellegaldau <148903629+isabellegaldau@users.noreply.github.com>
2024-04-17 20:51:36 +0000

    alpha

--- src/app.py
+++ src/app.py
@@ -2,3 +2,43 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model_file = "/workspaces/ml-web-app-ds/models/random_forest_model.pkl"
+class_dict = {
+    0: "Normal Weight",
+    1: "Overweight Level I",
+    2: "Overweight Level II",
+    3: "Overweight Level III",
+    4: "Obesity Type I",
+    5: "Obesity Type II",
+    6: "Obesity Type III"
+}
+
+# Load the trained model
+with open(model_file, 'rb') as file:
+    model = pickle.load(file)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # Get the input values from the form
+        age = float(request.form["age"])
+        gender = float(request.form["gender"])
+        height = float(request.form["height"])
+        weight = float(request.form["weight"])
+        fcvc = float(request.form["fcvc"])
+        ncp = float(request.form["ncp"])
+
+        # Make prediction
+        prediction = model.predict([[age, gender, height, weight, fcvc, ncp]])
+        pred_class = class_dict[prediction[0]]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction=pred_class)
+
+if __name__ == "__main__":
+    app.run(debug=True)
